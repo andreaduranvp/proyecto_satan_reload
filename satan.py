069a625a74a4a358b39c66db8316d7a9d57c217d
juan <jnfran92@gmail.com>
2020-12-13 00:02:21 -0300

    Initial commit

---
+++ satan.py
@@ -0,0 +1,16 @@
+
+print("My awesome code in Python -----------------")
+for i in range(1, 10):
+    print(str(i) + '.numbered')
+
+
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+# *******
+# ********
+# *********
+
